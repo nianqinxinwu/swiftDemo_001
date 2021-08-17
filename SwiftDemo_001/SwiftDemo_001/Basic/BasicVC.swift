4ea7dccd5ef6a70de238fd0c7e594e8a86447616
changyuan1 <changyuan1@yy.com>
2021-08-17 14:24:37 +0800

    pod更新

--- SwiftDemo_001/SwiftDemo_001/Basic/BasicVC.swift
+++ SwiftDemo_001/SwiftDemo_001/Basic/BasicVC.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 //import QMUIKit
+import SDWebImage
+import SnapKit
 
 class BasicVC : UIViewController {
     private lazy var titleLB : QMUILabel = {
@@ -25,4 +27,45 @@ class BasicVC : UIViewController {
         return contentLabel
     }()
     
+    private lazy var bottomLabel : QMUILabel = {
+        let label = QMUILabel()
+        return label
+    }()
+    
+    private lazy var headImg : UIImageView = {
+        let img = UIImageView()
+        return img
+    }()
+    
+    override func viewDidLoad() {
+        setupUI()
+    }
+    
+    private func setupUI() {
+        self.view.addSubview(headImg)
+        self.view.addSubview(titleLB)
+        self.view.addSubview(contentLabel)
+        
+        headImg.snp.makeConstraints { make in
+            
+        }
+        
+        titleLB.snp.makeConstraints { make in
+            
+        }
+        
+        contentLabel.snp.makeConstraints { make in
+            
+        }
+        
+    }
+}
+
+extension BasicVC {
+    public func updateTitle(title : String, content : String, imgUrl : String)
+    {
+        titleLB.text = title
+        headImg.sd_setImage(with: URL.init(string: imgUrl), completed: nil)
+        contentLabel.text = content
+    }
 }
